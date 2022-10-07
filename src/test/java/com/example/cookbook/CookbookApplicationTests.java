ac1ec005e9da12f9ff5fbb658de14ee182d542c0
zreit <zisis_relas@yahoo.com>
2022-10-07 16:02:12 +0200

    started working on tests

--- src/test/java/com/example/cookbook/CookbookApplicationTests.java
+++ src/test/java/com/example/cookbook/CookbookApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.cookbook;
 
+import com.example.cookbook.controller.AppUserController;
+import com.example.cookbook.controller.CookbookController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CookbookApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private AppUserController appUserController;
+	@Autowired
+	private CookbookController cookbookController;
 
+	@Test
+	void contextLoads() throws Exception {
+		assertThat(appUserController).isNotNull();
+		assertThat(cookbookController).isNotNull();
+	}
 }
---
+++ src/test/java/com/example/testingweb/HttpRequestTest.java
@@ -0,0 +1,13 @@
+package com.example.testingweb;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+}
